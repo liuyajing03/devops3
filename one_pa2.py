d14fce62b97f551e97cc361c6374d21155cb6ae0
liu-jian <a8966093@163.com>
2016-09-21 11:36:22 +0800

    更新作业，兼容汉字

--- one_pa2.py
+++ one_pa2.py
@@ -1,19 +1,34 @@
-x = "xxcicrbnclsuiycbfeej1938e47rrceicboof7o845yfyljcns;sjcp98qeoqyf43bbfljnlfcuhehrfuh"
+# coding=utf-8
+import unittest, pdb
+
+x = "哈哈哈哈哈哈哈哈xxcicrbnclsuiycbfeej1938e47rrceicboof7o845yfyljcns;sjcp98qeoqyf43bbfljnlfcuhehrfuh"
 
 def codeCount(char_in):
+	char = unicode(char_in,"utf-8")
 	b = {}
-	for i,ch in enumerate(char_in):
+	for i,ch in enumerate(char):
 		if ch not in b:
 			b[ch] = 1
 		else:
 			b[ch] += 1
 	c = sorted(b.iteritems(), key = lambda asd:asd[1], reverse = True)
 	i = 0
+	d = []
 	for res in c:
 		if i <=9:
-			print c[i]
+			d.append(c[i])
 			i += 1
 		else:
 			break
+	return d
 
-codeCount(x)
+
+class TestCameltoUnderline(unittest.TestCase):
+	def test(self):
+		char_in = "aaaaccc4444444你"
+		res = [(u'4', 7), (u'a', 4), (u'c', 3),(u'你', 1)]
+		self.assertEqual(codeCount(char_in), res)
+
+if __name__ == "__main__":
+	unittest.main()
+
